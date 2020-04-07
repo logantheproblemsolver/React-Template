d58cc7b020b075d710d175ff130a70c4f78a239f
unknown <mrloganellis@gmail.com>
2020-04-07 13:38:27 -0400

    created react template

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a template to start React applications!
+
+There is a React Hamburger Menu npm already downloaded:
+to use it- https://www.npmjs.com/package/react-hamburger-menu
---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+import HamburgerMenu from 'react-hamburger-menu';
+import './NavBar.css'
+
+
+class NavBar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            open: false,
+            showList: false
+        }
+    }
+
+
+    handleClick() {
+        this.setState({
+            open: !this.state.open,
+            showList: !this.state.showList
+        });
+    }
+
+      
+    render() {          
+        
+        const {showList} = this.state
+
+        // This navigation is so it's easier to navigate pages, and also it can be scaleable or resuable 
+        return (
+            <nav className="navigation">
+                <h1 className="title left"><Link to="/">Community Artists</Link></h1>
+                <ul className={`${showList ? "nav-menu" : "nav-menu hidden"}`}>
+                    <li className="nav-list" ><Link to="/path">Path Name</Link></li>
+                    <li className="nav-list" ><Link to="/path">Path Name</Link></li>
+                    {/* you can add more li just like the one above */}
+                </ul>
+                <div className="nav-toggle">
+                    <HamburgerMenu
+                        isOpen={this.state.open}
+                        menuClicked={this.handleClick.bind(this)}
+                        width={18}
+                        height={15}
+                        animationDuration={.1}
+                        className="hamburger"
+                    />     
+                </div>
+
+
+            </nav>     
+        )
+
+    }
+}
+
+export default NavBar;
